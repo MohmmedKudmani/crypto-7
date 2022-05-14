09638c689725e0c0f3a1ecd39e2a55ce509de13f
Mohammed Kudmani <m7mdsy976@gmail.com>
2022-05-14 20:29:23 +0300

    added api key to env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['assets.coingecko.com', 'images.cryptocompare.com'],
+  },
+  env: {
+    API_KEY: 'd2e8e5578df8aee90cd12cbde129e59dba04407cb12711993c81db551ddafa92',
   },
 }
 
