f6f52dc5b78ddce84b5d66c4b4eb37fb17cb28bf
Mohammed Kudmani <m7mdsy976@gmail.com>
2022-05-19 17:23:57 +0300

    added export full static html

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['assets.coingecko.com', 'images.cryptocompare.com'],
+    loader: 'akamai',
+    path: '',
   },
   env: {
     API_KEY: 'd2e8e5578df8aee90cd12cbde129e59dba04407cb12711993c81db551ddafa92',
